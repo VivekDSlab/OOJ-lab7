43bb1fc9fe99817c7da9d44437c331dba19866dc
VivekDSlab <vivekv.cs24@bmsce.ac.in>
2024-11-12 12:55:58 +0530

    Create Concatenate.java

---
+++ Concatenate.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Concatenate{
+public static void main(String srgs[]){
+
+String hi = "Hi, ";
+String mom = "mom.";
+
+String way1 = hi + mom;
+String way2 = hi.concat(mom);
+
+System.out.println(hi);
+System.out.println(mom);
+System.out.println(way1);
+System.out.println(way2);
+}}
